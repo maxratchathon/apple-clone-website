3504c1493b471102fb67dee1c2059e3cdf809324
Maxmerock123 <Maxmerock@gmail.com>
2024-05-06 15:49:03 +0700

    add imagelist for items scrolling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,20 @@ import styles from "./page.module.css";
 import Button from '@mui/material/Button';
 import { Container, fontFamily } from "@mui/system";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Box, Link, Typography } from "@mui/material";
+import { Box, ImageList, ImageListItem, Link, Typography } from "@mui/material";
 import { useEffect } from "react";
+
+import item1 from "../../public/home-items/mac.png"
+import item2 from "../../public/home-items/iphone.png"
+import item3 from "../../public/home-items/ipad.png"
+import item4 from "../../public/home-items/apple-watch.png"
+import item5 from "../../public/home-items/airpod.png"
+import item6 from "../../public/home-items/airtag.png"
+import item7 from "../../public/home-items/apple-tv.png"
+import item8 from "../../public/home-items/home-pod.png"
+import item9 from "../../public/home-items/others.png"
+import item10 from "../../public/home-items/gift-card.png"
+
 
 
 
@@ -31,6 +43,21 @@ export default function Home() {
 
   }
 
+  const itemData = [
+    { img: item1, title: 'Mac' },
+    { img: item2, title: 'iPhone' },
+    { img: item3, title: 'iPad' },
+    { img: item4, title: 'Apple Watch' },
+    { img: item5, title: 'AirPod' },
+    { img: item6, title: 'AirTag' },
+    { img: item7, title: 'Apple TV' },
+    { img: item8, title: 'HomePod' },
+    { img: item9, title: 'อุปกรณ์เสริม' },
+    { img: item10, title: 'บัตรของขวัญ Apple Store' }
+  ];
+
+  console.log(itemData[3].img.src)
+
 
 
 
@@ -39,12 +66,12 @@ export default function Home() {
   return (
     <Container >
       <Box sx={{ display: "flex", justifyContent: 'center', gap: "10px" }}>
-        {Object.entries(navBar).map(([key, value]) => (<Typography key={key}>{value}</Typography>))}
+        {Object.entries(navBar).map(([key, value]) => (<Typography sx={{ fontSize: '15px' }} key={key}>{value}</Typography>))}
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: "10vh" }}>
         <Box sx={{ maxWidth: '600px' }}>
-          <Typography variant="h3">ร้านของเรา{' '}
-            <Typography variant="h3" sx={{ color: 'grey', display: 'inline' }}>
+          <Typography variant="h3" component='span'>ร้านของเรา{' '}
+            <Typography variant="h3" component='span' sx={{ color: 'grey', display: 'inline' }}>
               คือที่ที่ดีที่สุดในการซื้อผลิตภัณฑ์ที่คุณรัก
             </Typography>
           </Typography>
@@ -62,6 +89,25 @@ export default function Home() {
           </Box>
         </Box>
       </Box>
+
+      {/* Image List */}
+      <Box >
+        <ImageList sx={{ flexWrap: 'nowrap', }} cols={itemData.length} >
+          {itemData.map((item) => (
+            <ImageListItem key={item.title} sx={{ mx: 2 }}>
+              <img
+                src={item.img.src}
+                alt={item.title}
+                loading="lazy"
+                style={{ width: '120px', objectFit: 'contain' }}
+              />
+              <Typography align="center">{item.title}</Typography>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
+
+
     </Container >
   );
 }
